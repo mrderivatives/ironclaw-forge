672ef0e98350b6ab6091a7d5c4bfa4db2ccb6192
Beep Boop <beepboop@Beeps-MacBook-Pro.local>
2026-03-13 11:27:54 -0400

    feat: add NEAR wormhole-wrapped SPL mint to TOOLS.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,6 +53,7 @@ Token mint addresses:
 - SOL: So11111111111111111111111111111111111111112
 - USDC: EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v
 - BONK: DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263
+- NEAR (Wormhole-wrapped on Solana): 4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R
 
 Your wallet address: ${AGENT_WALLET}
 TOOLSEOF
